671b5c0fe0b5dc71aa36ca1e8881c23240087eaf
Tek-nr <53139667+Tek-nr@users.noreply.github.com>
2021-11-02 19:50:56 +0300

    Add files via upload

---
+++ Polymorphism2.java
@@ -0,0 +1,50 @@
+/*class Animal{
+    private String name;
+    public Animal(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    
+    public String speak(){
+        return "Animal is speaking";
+    }
+}
+
+class Cat extends Animal{
+    public  Cat(String name){
+        super(name);
+    }
+    
+    @Override
+    public String speak(){
+        return getName() + ": crrrr";
+    }
+}
+
+class Dog extends Animal{
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String speak() {
+        return getName()+": havhav";
+    }
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}*/
+
